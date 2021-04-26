633108c3314dc7c331e467774d50ac13f80da322
Vishweshwar Palleboina <vishu0207@gmail.com>
2021-04-26 17:05:17 +0530

    Moving constants to a diff file

---
+++ src/main/java/com/vmware/gerrit/plugins/commitvalidator/entities/Constants.java
@@ -0,0 +1,23 @@
+package com.vmware.gerrit.plugins.commitvalidator.entities;
+
+public class Constants {
+    // Config constants
+    public static final String CONFIG_FILENAME_WITHOUT_EXTN = "commit-validator";
+    public static final String CONFIG_SECTION_JIRA_ENDPOINT = "endpoint-jira";
+    public static final String CONFIG_SECTION_TEMPLATE_ENTRY = "template-entry";
+    public static final String CONFIG_SECTION_COMMIT_TEMPLATE = "commit-template";
+    public static final String CONFIG_SECTION_PROJECT_MAPPINGS = "project-mappings";
+    public static final String CONFIG_ENDPOINT_URL = "url";
+    public static final String CONFIG_ENDPOINT_USERNAME = "username";
+    public static final String CONFIG_ENDPOINT_PASSWORD = "password";
+    public static final String CONFIG_ENABLED = "enabled";
+    public static final String CONFIG_TEMPLATE_ENTRY_KEY = "key";
+    public static final String CONFIG_TEMPLATE_ENTRY_VALUE = "value";
+    public static final String CONFIG_TEMPLATE_ENTRY_TYPE = "type";
+    public static final String CONFIG_TEMPLATE_ENTRY_VALIDATE_AGAINST_ENDPOINT = "validateAgainstEndpoint";
+    public static final String CONFIG_TEMPLATE_ENTRY_ENDPOINT_NAME = "endpointName";
+    public static final String CONFIG_TEMPLATE_ENTRY_ENDPOINT_TYPE = "endpointType";
+    public static final String CONFIG_COMMIT_TEMPLATE_MANDATORY_ENTRY = "mandatoryEntry";
+    public static final String CONFIG_COMMIT_TEMPLATE_OPTIONAL_ENTRY = "optionalEntry";
+    public static final String CONFIG_PROJECT = "project";
+}
